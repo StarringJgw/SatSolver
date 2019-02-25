6bd3ae0d7c0febc4a7643ec16c48b87da581c6fc
jgw <starring_jgw@qq.com>
2019-02-25 16:09:49 +0800

    Sudoku Board/Test Version5

--- SudokuSolver.h
+++ SudokuSolver.h
@@ -7,7 +7,8 @@
 
 #include "myList.h"
 #include "myVector.h"
-
+#include "cmath"
+#include "iostream"
 typedef myList<myList<int >> Formula;
 typedef myList<int> Clause;
 typedef Clause *Iterator;
@@ -16,16 +17,56 @@ typedef myVector<int> Solution;
 
 typedef int Board[9][9];
 
-class Sudoku {
+class SudokuSolver {
 private:
-    Formula formula;
 public:
-    Sudoku() {
+    Formula formula;
+
+    SudokuSolver() {
         formula = *new Formula;
+        //ensure each entry have one number
+        for (int i = 1; i <= 729; i = i + 9) {
+            int current[9] = {i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8};
+            PushBasicRule(current);
+        }
+        cout << formula.Size() << endl;
+        //ensure each row
+        for (int row = 1; row <= 9; row++) {
+            int currentNum = (row - 1) * 81 + 1;
+            for (int i = 1; i <= 9; currentNum++, i++) {
+                int current[9] = {currentNum, currentNum + 9, currentNum + 18, currentNum + 27, currentNum + 36,
+                                  currentNum + 45, currentNum + 54, currentNum + 63, currentNum + 72};
+                PushBasicRule(current);
+            }
+        }
+        cout << formula.Size() << endl;
+        //ensure each column
+        for (int column = 1; column <= 9; column++) {
+            int currentNum = (column - 1) * 9 + 1;
+            for (int i = 1; i <= 9; currentNum++, i++) {
+                int current[9] = {currentNum, currentNum + 81, currentNum + 81 * 2, currentNum + 81 * 3,
+                                  currentNum + 81 * 4, currentNum + 81 * 5, currentNum + 81 * 6, currentNum + 81 * 7,
+                                  currentNum + 81 * 8};
+                PushBasicRule(current);
+            }
+        }
+        cout << formula.Size() << endl;
+        //ensure each grid
+        for (int grid = 1; grid <= 9; grid++) {
+            int currentNum = ((grid - 1) / 3) * 243 + ((grid - 1) % 3) * 27 + 1;
+            for (int i = 1; i <= 9; currentNum++, i++) {
+                int current[9] = {currentNum, currentNum + 9, currentNum + 18, currentNum + 81, currentNum + 9 + 81,
+                                  currentNum + 18 + 81, currentNum + 81 * 2, currentNum + 9 + 81 * 2,
+                                  currentNum + 18 + 81 * 2};
+                PushBasicRule(current);
+            }
+        }
     }
 
-    Formula PushBasicRule(int target[9]) {
+    void PushBasicRule(int target[9]) {
+        Clause least = *new Clause;
         for (int i = 0; i < 9; i++) {
+            least.push_back(target[i]);
             for (int i1 = i + 1; i1 < 9; i1++) {
                 Clause temp = *new Clause;
                 temp.push_back(-target[i]);
@@ -33,6 +74,7 @@ public:
                 formula.push_back(temp);
             }
         }
+        formula.push_back(least);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "chrono"
 #include "CnfParser.h"
 #include "regex"
+#include "SudokuSolver.h"
 //#include "algorithm"
 using namespace std;
 
@@ -16,8 +17,9 @@ using namespace std;
 int main(void) {
     auto satSolver = *new SatSolver;
     auto cnfParser = *new CnfParser;
-
-    Formula formula = cnfParser.readCnf("Base_B1.cnf");
+    auto sudokuSolver = *new SudokuSolver;
+    Formula formula = cnfParser.readCnf("base_pwr.cnf");
+//    cout << formula.Size()<< endl;
     Solution solution = satSolver.Solve(formula);
     cnfParser.outputSolution(satSolver.status, solution, satSolver.time);
 
